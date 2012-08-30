6a23f6a606f942fcaab7e1fe5026c5051a9a3d67
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2012-08-30 11:28:42 +0200

    infrastructure functions

--- src/robogo/core/board.go
+++ src/robogo/core/board.go
@@ -4,6 +4,15 @@ package core
 // and the robot on the square in the top four bits.
 type Square byte
 
+// "func (s Square) Color() byte" is also possible
+func Color(s Square) byte {
+	return byte((s >> 4) & 7)
+}
+
+func Walls(s Square) byte {
+	return byte(s & 0x0f)
+}
+
 // A board of size n containing a slice of n^2 Squares.
 type Board struct {
 	size uint
@@ -13,3 +22,27 @@ type Board struct {
 func NewBoard(n uint) *Board {
 	return &Board{n, make([]Square, n * n)}
 }
+
+// Offset into the field slice.
+type Location uint
+
+func (b *Board) X(l Location) uint {
+	return uint(l) % b.size
+}
+
+func (b *Board) Y(l Location) uint {
+	return uint(l) / b.size
+}
+
+func (b *Board) XY(x, y uint) Location {
+	return Location(y * b.size + x)
+}
+
+// Walls and directions.
+type Direction byte
+const (
+	NORTH Direction = 1 << iota
+	EAST
+	SOUTH
+	WEST
+)
--- src/robogo/core/board_test.go
+++ src/robogo/core/board_test.go
@@ -12,3 +12,45 @@ func TestBoardConstructor(t *testing.T) {
 		t.Errorf("Number of fields was %v, expected %v", l, n2)
 	}
 }
+
+func TestColorOfSquare(t *testing.T) {
+	const color, walls = 3, 2
+	const x = 16 * color + walls
+	if c := Color(x); c != color {
+		t.Errorf("Color was %v, expected %v", c, color)
+	}
+	if w := Walls(x); w != walls {
+		t.Errorf("Walls was %v, expected %v", w, walls)
+	}
+}
+
+func TestX(t *testing.T) {
+	b := NewBoard(5)
+	x17 := uint(2)
+	if x := b.X(17); x != x17 {
+		t.Errorf("X was %v, expected %v", x, x17)
+	}
+}
+
+func TestY(t *testing.T) {
+	b := NewBoard(5)
+	y17 := uint(3)
+	if y := b.Y(17); y != y17 {
+		t.Errorf("Y was %v, expected %v", y, y17)
+	}
+}
+
+func TestXY(t *testing.T) {
+	b := NewBoard(5)
+	xy := Location(17)
+	if n := b.XY(2, 3); xy != n {
+		t.Errorf("XY was %v, expected %v", n, xy)
+	}
+}
+
+func TestWEST(t *testing.T) {
+	west := Direction(8)
+	if WEST != west {
+		t.Errorf("WEST was %v, expected %v", WEST, west)
+	}
+}

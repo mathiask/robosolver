f755c01d4408f023320e874b4bf5e83e45e083cd
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2012-09-17 22:25:36 +0200

    support for Android (iPhone only partly working)

--- go/src/robogo/gui/gui.go
+++ go/src/robogo/gui/gui.go
@@ -1,65 +1,65 @@
 package gui
 
 import (
-	"fmt"
-	"strings"
-	"net/http"
-	"html/template"
-	"robogo/core"
+    "fmt"
+    "strings"
+    "net/http"
+    "html/template"
+    "robogo/core"
 )
 
 const MAX_DEPTH = 15
 
 type Cell struct {
-	Walls core.Square
-	Id string
+    Walls core.Square
+    Id string
 }
 
 func Init() {
-	http.HandleFunc("/", mainHandler)
-	http.HandleFunc("/solve", solveHandler)
+    http.HandleFunc("/", mainHandler)
+    http.HandleFunc("/solve", solveHandler)
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
-	mainTemplate.Execute(w, board(core.StandardBoard()))
+    mainTemplate.Execute(w, board(core.StandardBoard()))
 }
 
 func board(b *core.Board) [][]Cell {
-	n := b.Size()
-	result := make([][]Cell, n)
-	for y := uint(0); y < n; y++ {
-		result[y] = make([]Cell, n)
-		for x := uint(0); x < n; x++ {
-			result[y][x] = Cell{ b.WallsAt(x, y), fmt.Sprintf("%v_%v", x, y) }
-		}
-	}
-	return result
+    n := b.Size()
+    result := make([][]Cell, n)
+    for y := uint(0); y < n; y++ {
+        result[y] = make([]Cell, n)
+        for x := uint(0); x < n; x++ {
+            result[y][x] = Cell{ b.WallsAt(x, y), fmt.Sprintf("%v_%v", x, y) }
+        }
+    }
+    return result
 }
 
 func solveHandler(w http.ResponseWriter, r *http.Request) {
-	robots := new([4][2]uint)
-	for i := 0; i < 4; i++ {
-		fmt.Sscanf(r.FormValue(fmt.Sprintf("robot%v", i + 1)),
-			"%v_%v", &robots[i][0], &robots[i][1])
-	}
-	var tx, ty uint
-	fmt.Sscanf(r.FormValue("target"), "%v_%v", &tx, &ty)
-	b := core.StandardBoard().Reset(robots)
-	p := core.NewPosition(b, b.Location(tx, ty))
-	fmt.Fprintf(w, solve(p, robots))
+    robots := new([4][2]uint)
+    for i := 0; i < 4; i++ {
+        fmt.Sscanf(r.FormValue(fmt.Sprintf("robot%v", i + 1)),
+            "%v_%v", &robots[i][0], &robots[i][1])
+    }
+    var tx, ty uint
+    fmt.Sscanf(r.FormValue("target"), "%v_%v", &tx, &ty)
+    b := core.StandardBoard().Reset(robots)
+    p := core.NewPosition(b, b.Location(tx, ty))
+    fmt.Fprintf(w, solve(p, robots))
 }
 
 func solve(p *core.Position, robots *[4][2]uint) string {
-	for depth := uint(2); depth <= MAX_DEPTH; depth++ {
-		fmt.Printf("Solving with max depth %v...\n", depth);
-		if p.Solve(depth) {
-			fmt.Printf("Solved!\n");
-			return strings.Join(p.Move(), ", ")
-		}
-		p.Reset(robots)
-	}
-	fmt.Printf("Search aborted!\n");
-	return fmt.Sprintf("No solution found at depth %v!", MAX_DEPTH)
+    for depth := uint(2); depth <= MAX_DEPTH; depth++ {
+        fmt.Printf("Solving with max depth %v...\n", depth);
+        if p.Solve(depth) {
+            fmt.Printf("Solved!\n");
+            return strings.Join(p.Move(), ", ")
+        }
+        p.Reset(robots)
+    }
+    fmt.Printf("Search aborted!\n");
+    return fmt.Sprintf("No solution found at depth %v!", MAX_DEPTH)
 }
 
 var mainTemplate = template.Must(template.New("test").Parse(mainTemplateHTML))
@@ -69,54 +69,53 @@ const mainTemplateHTML = `
 <html lang="en">
 <head>
 <meta charset="utf-8"/>
-<script src="static/scripts/jquery-1.7.2.min.js">
-</script>
-<script src="static/scripts/jquery-ui-1.8.18.custom.min.js">
-</script>
+<script src="static/scripts/jquery-1.7.2.min.js"></script>
+<script src="static/scripts/jquery-ui-1.8.18.custom.min.js"></script>
+<script src="static/scripts/jquery.ui.touch-punch.min.js"></script>
 <script>
 $(function(){
   var robogo = {
-	  positions: {}
-	},
-	solutionDiv = $("#solution"),
-	positionsComplete = function() {
-	  var p = robogo.positions;
-	  if (!p["target"]) return false;
-	  for (var i = 1; i <= 4; i++) {
-		if (!p["robot" + i]) return false;
-	  }
-	  return true;
-	},
-	showSolving = function() {
-	  solutionDiv.text("Solving...");
-	},
-	solve = function() {
-	  $.get("solve", robogo.positions, function(data) {
-		solutionDiv.text(data);
-	  });
-	};
+      positions: {}
+    },
+    solutionDiv = $("#solution"),
+    positionsComplete = function() {
+      var p = robogo.positions;
+      if (!p["target"]) return false;
+      for (var i = 1; i <= 4; i++) {
+        if (!p["robot" + i]) return false;
+      }
+      return true;
+    },
+    showSolving = function() {
+      solutionDiv.text("Solving...");
+    },
+    solve = function() {
+      $.get("solve", robogo.positions, function(data) {
+        solutionDiv.text(data);
+      });
+    };
   solutionDiv.ajaxError(function() {
-	$(this).text( "AJAX error!" );
+    $(this).text( "AJAX error!" );
   });
   $("img").draggable({revert: "invalid"});
   $(".box").droppable({drop: function(event, ui) {
-	  var target = $(this),
-		id = ui.draggable.attr("id");
-	  if (robogo.positions[id]) {
-		$("#" + robogo.positions[id]).droppable("option", "disabled", false);
-	  }
-	  robogo.positions[id] = this.id;
-	  ui.draggable.detach().css({top: 0, left: 0}).appendTo(target);
-	  $("#" + this.id).droppable("option", "disabled", true);
-	},
-	hoverClass: "ui-state-hover"});
+      var target = $(this),
+        id = ui.draggable.attr("id");
+      if (robogo.positions[id]) {
+        $("#" + robogo.positions[id]).droppable("option", "disabled", false);
+      }
+      robogo.positions[id] = this.id;
+      ui.draggable.detach().css({top: 0, left: 0}).appendTo(target);
+      $("#" + this.id).droppable("option", "disabled", true);
+    },
+    hoverClass: "ui-state-hover"});
   $("button").click(function(){
-	if (positionsComplete()) {
-	  showSolving();
-	  solve();
-	} else {
-	  alert("Please place target and all robots!");
-	}
+    if (positionsComplete()) {
+      showSolving();
+      solve();
+    } else {
+      alert("Please place target and all robots!");
+    }
   });
 });
 </script>
@@ -213,7 +212,7 @@ button { margin: 2ex 0px 2ex 0px; }
 {{ range . }}
   <div><!--
   {{ range . }}
-	--><div class="box box{{.Walls}}" id="{{.Id}}"></div><!--
+    --><div class="box box{{.Walls}}" id="{{.Id}}"></div><!--
   {{ end }}
   --></div>
 {{ end }}
